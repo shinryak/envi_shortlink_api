ab2919f65bfc9a97ee09876db3d4132fe132e029
shinryak <admin@shinryak.com>
2021-03-18 21:26:35 +0700

    add virtual redirect url

--- model/shortLink.js
+++ model/shortLink.js
@@ -1,11 +1,23 @@
 const { Schema, model } = require('mongoose');
 
-const ShortLinkSchema = Schema({
-  url: Schema.Types.String,
-  query: { type: Schema.Types.String, minLength: 6 },
-  user: Schema.Types.ObjectId,
-  isDeleted: { type: Schema.Types.Boolean, default: false },
-  isDisable: { type: Schema.Types.Boolean, default: false },
+const ShortLinkSchema = Schema(
+  {
+    url: Schema.Types.String,
+    query: { type: Schema.Types.String, minLength: 6 },
+    user: Schema.Types.ObjectId,
+    isDeleted: { type: Schema.Types.Boolean, default: false },
+    isDisable: { type: Schema.Types.Boolean, default: false },
+  },
+  {
+    toObject: {
+      virtuals: true,
+    },
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+ShortLinkSchema.virtual('redirect').get(function () {
+  return process.env.REDIRECT_HEAD + this.query;
 });
-
 module.exports = model('ShortLink', ShortLinkSchema);
